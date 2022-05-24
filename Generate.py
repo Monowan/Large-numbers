a78a1df8ed046d116fcb1c6c06b08f8acf7fd79d
Monowan <93907627+Monowan@users.noreply.github.com>
2022-05-24 21:35:18 +0300

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -5,6 +5,7 @@ num = int(2)
 
 while bit != 8192:
         i = num**bit
-        bit = bit * 2
         number = random.randint(0, i-1)
         print(bit,"-rand:",number)
+        bit = bit * 2
+        
